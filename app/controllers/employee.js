4a9cf6727fd0867ef277bea95f34c09da0cc222b
Bharat Marewad <b.marewad@gmail.com>
2016-08-23 01:31:00 +0530

    delete employee code changes

--- app/controllers/employee.js
+++ app/controllers/employee.js
@@ -1,27 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    employeeResult: null,
-    pageNumber:0,
-    pageSize: 2,
-    pages: Ember.computed('content', 'pageSize',function() {
-        var pages = [];
-        var arrangedContent = this.get('model') || [];
-        while(arrangedContent.length > 0) {
-          pages.push(arrangedContent.sllice(0, this.get('pageSize')));
-        }
-        return pages;
-    }),
-    paginatedContent: Ember.computed('pages', 'pageNumber',function() {
-      console.log(this.get('pages')[this.get('pageNumber')]);
-        return this.get('pages')[this.get('pageNumber')];
-    }),
-    // actions: {
-    //   employeeList() {
-    //     console.log('actioncalled');
-    //      let employeeList = this.get('store').findAll('employee').then((employeeList)=> {
-    //        this.set('employeeResult', employeeList);
-    //      });
-    //   }
-    // }
+    actions: {
+      removeEmployee(employee) {
+         this.get('store').findRecord('employee', employee.id).then((employee)=> {
+           employee.destroyRecord();
+         });
+      },
+    }
 });
